fac5746ba1ded6fc23b48f125db99afd6ff82fc6
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-02-23 19:29:07 +0530

    speaker links

--- Speakers.js
+++ Speakers.js
@@ -1,50 +1,50 @@
 const events = [
   {
     name: "T D Ramakrishnan",
-    img: "./Speakers/hi.jpeg",
+    img: "./allAssets/Speakers/speakersFinal/s1.webp",
     desc: `T D Ramakrishnan is a novelist, translator, and retired Chief Controller in Southern Railway. He has authored two best-selling Malayalam novels: Francis Itty Cora and Sugandhi Enna Andal Devanayaki. He is a recipient of the Kerala Sahitya Akademi Award and Vayalar Award.`,
     role: "Novelist | Bramayugam script writer ",
   },
   {
     name: "Anu Suraj",
-    img: "./Speakers/AnuSuraj.jpeg",
+    img: "./allAssets/Speakers/speakersFinal/s2.webp",
     desc: `Dr Anu Suraj is a trauma-informed psychologist, social worker, and child rights advocate. She is the founder of Swaraksha and Aatmasaukhya, dedicated to mental health and child protection.`,
     role: "Psychologist",
   },
   {
     name: "Salini",
-    img: "./Speakers/salini.jpeg",
+    img: "./allAssets/Speakers/speakersFinal/s4.webp",
     desc: `Salini is a prominent radio jockey from Kerala, best known for hosting Club FM's Star Jam show. In addition to her radio career, she is an author, having written the Malayalam book Poochakkuru, published in 2022.`,
     role: "RJ | Author ",
   },
   {
     name: "Anusha Andrews",
-    img: "./Speakers/AnushaAndrews.jpeg",
+    img: "./allAssets/Speakers/anusha.JPG",
     desc: `Anusha Andrews is a journalist recognized for her fearless political stance and insightful reporting. Passionate about social change, she actively addresses issues of gender and power, advocating for women in the political society.`,
     role: "Journalist",
   },
   {
     name: "Dr. Fathima Saheer",
-    img: "./Speakers/dr.jpg",
+    img: "./allAssets/Speakers/dr.jpg",
     desc: `Dr. Fathima Saheer is a consultant pediatrician, parenting expert, and researcher. She shares insights on child development, positive parenting, and healthcare challenges through her YouTube channel and media appearances.`,
     role: "Pediatrician | Parenting expert",
   },
   {
     name: "C. Prathapmohan Nair",
-    img: "./Speakers/prathapmohan.jpeg",
+    img: "./allAssets/Speakers/prathapmohan.jpeg",
     desc: `C. Prathapmohan Nair is an urban planning, infrastructure, and sustainability expert with 25+ years of experience. A Certified PPP Specialist, he has worked with the World Bank, PwC, and ADB and currently consults for the UN (UNCCD) on sustainability and green financing. He has held leadership roles, including Executive Director (i/c) of KSID, and teaches at Rajagiri Business School and IIIC. A Distinguished Toastmaster, he also serves as District Administration Manager for District 121.`,
     role: "Urban planner",
   },
   {
     name: "Gouri Krishna",
-    img: "./Speakers/GouriKrishna.jpeg",
+    img: "./allAssets/Speakers/speakersFinal/s3.webp",
     desc: `Gouri Krishna is an Integrative Nutrition Health Coach and Nutritionist with a triple major in Nutrition, Chemistry, and Human Development
                 from Mount Carmel College. Certified by the Institute for Integrative Nutrition (IIN), she is a Senior Nutritionist at Harmony Wellness Concepts and shares wellness insights via her Instagram, @umminity.`,
     role: "Integrative nutrition health coach | Nutritionist",
   },
   {
     name: "Rahib Mohamed",
-    img: "./Speakers/RahibMohamed.jpeg",
+    img: "./allAssets/Speakers/SRA07349.JPG",
     desc: `Rahib Mohamed, known as "Bheegaran," is a lifestyle wellness coach and fitness innovator from Kochi, India. A former civil engineer, he began his fitness journey in 2017, focusing on holistic well-being. As the founder of DayZero Wellness and co-founder of The Sol Wellness and Padel High Kochi, he promotes sustainable lifestyle transformations. His philosophy, "I train your MIND, your BODY follows," emphasizes mental strength as the key to physical fitness, inspiring a global wellness community.`,
     role: "Lifestyle wellness coach",
   },
